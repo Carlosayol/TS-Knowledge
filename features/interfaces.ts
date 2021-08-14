197b99bf2929f24c576c5ed9fd2c75259f54e448
Carlosayol <Carlosaolar@gmail.com>
2021-08-14 10:46:11 -0500

    create interface

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,10 +1,16 @@
+interface Vehicle {
+    name: string;
+    year: number;
+    broken: boolean;
+}
+
 const oldCivic = {
     name: 'Civic',
     year: 2000,
     broken: true
 }
 
-const printVehicle = (vehicle: {name: string; year: number; broken: boolean}): void => {
+const printVehicle = (vehicle: Vehicle): void => {
     console.log(vehicle.name);
     console.log(vehicle.year);
     console.log(vehicle.broken);
