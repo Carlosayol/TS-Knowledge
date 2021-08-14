fdb887987e3a1359a813481fe3a6bb4f4d25413a
Carlosayol <Carlosaolar@gmail.com>
2021-08-14 10:42:03 -0500

    create class without interface, long type annotation

---
+++ features/interfaces.ts
@@ -0,0 +1,13 @@
+const oldCivic = {
+    name: 'Civic',
+    year: 2000,
+    broken: true
+}
+
+const printVehicle = (vehicle: {name: string; year: number; broken: boolean}): void => {
+    console.log(vehicle.name);
+    console.log(vehicle.year);
+    console.log(vehicle.broken);
+}
+
+printVehicle(oldCivic);
