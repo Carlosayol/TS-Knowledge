bf40276fadd81e526412eccfadae2dc2aaaca980
Carlosayol <Carlosaolar@gmail.com>
2021-08-22 18:46:27 -0500

    test new functionality

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -5,6 +5,8 @@ import { CustomMap } from './CustomMap';
 
 const user = new User();
 const customMap = new CustomMap('map');
+const company = new Company();
 
 customMap.addMarker(user);
+customMap.addMarker(company);
 
