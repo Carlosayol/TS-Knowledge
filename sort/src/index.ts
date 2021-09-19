4fa92273ee48ff1d80a37cf9bfe01b34605ba494
Carlosayol <Carlosaolar@gmail.com>
2021-09-19 18:48:41 -0500

    added numbercollection class to encapsulate collection functions

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,5 +1,7 @@
 import { Sorter } from "./Sorter"
+import { NumberCollection } from "./NumbersCollection"
 
-const sorter = new Sorter([10, 3, -5, 0])
+const numberCollection = new NumberCollection([10, 3, -5, 0])
+const sorter = new Sorter(numberCollection)
 sorter.sort()
 console.log(sorter.collection)
