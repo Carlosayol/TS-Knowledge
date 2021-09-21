77307d5208d71c1f29dec5a9c9760d10054cde11
Carlosayol <Carlosaolar@gmail.com>
2021-09-21 12:23:52 -0500

    add linked list class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,11 +1,13 @@
 import { Sorter } from "./Sorter"
 import { NumberCollection } from "./NumbersCollection"
 import { CharactersCollection } from "./CharactersCollection"
+import { LinkedList } from "./LinkedList"
 
-const numberCollection = new NumberCollection([10, 3, -5, 0])
-const charactersCollection = new CharactersCollection("Xaayb")
-const sorter = new Sorter(numberCollection)
-const sorter2 = new Sorter(charactersCollection)
-sorter.sort()
-sorter2.sort()
-console.log(sorter.collection)
+// const numberCollection = new NumberCollection([10, 3, -5, 0])
+// const charactersCollection = new CharactersCollection("Xaayb")
+// const sorter = new Sorter(numberCollection)
+// const sorter2 = new Sorter(charactersCollection)
+// sorter.sort()
+// sorter2.sort()
+
+//console.log(charactersCollection.data)
