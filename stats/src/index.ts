a2493af27c131ad60dfc89b36c1c6965c41f82a9
Carlosayol <Carlosaolar@gmail.com>
2021-10-30 13:23:39 -0500

    create first analyzer for wins

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -7,12 +7,3 @@ reader.read()
 const matches = reader.data
 
 // enum - enumeration is used to signaling to other engineers about a group of close related values
-
-let manUnitedWins = 0
-for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin){
-    manUnitedWins++
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin){
-    manUnitedWins++
-  }
-}
