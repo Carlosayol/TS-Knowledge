47f07b01b027df0a95386822f1930c49ccf3ff08
Carlosayol <Carlosaolar@gmail.com>
2021-10-31 11:31:00 -0500

    use summary in index.ts

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,9 +1,19 @@
 import { MatchReader } from "./MatchReader"
-import { MatchResult } from "./MatchResult"
+import { ConsoleReport } from "./reportTargets/ConsoleReport"
+import { WinsAnalysis } from "./analyzers/WinsAnalysis"
+import { Summary } from "./Summary"
+import { Console } from "console"
 
 const reader = new MatchReader('football.csv')
 reader.read()
 
 const matches = reader.data
+
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+)
+
+summary.buildAndPrintReport(matches)
 
 // enum - enumeration is used to signaling to other engineers about a group of close related values
