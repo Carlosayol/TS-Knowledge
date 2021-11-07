45cbe0aa6b93f84b1a311bace6c86ab1ec1d0c21
Carlosayol <Carlosaolar@gmail.com>
2021-11-07 12:52:13 -0500

    test fetch functionality

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,19 +1,6 @@
-import axios from 'axios'
 import { User } from './models/User'
 
-// const user = new User({ name: 'myname', age: 20 })
+const user = new User({ id: 1 })
 
-// user.set({ name: 'newName', age: 9999 })
-
-// console.log(user.get('name'))
-
-// user.on('change', () => {
-//   console.log('Change mimis')
-// })
-
-// user.trigger('change')
-
-axios.post('http://localhost:3000/users' , {
-  name: 'myname',
-  age: 20
-})
+user.fetch()
+console.log(user)
