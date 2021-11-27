3fa081483b2b040f6811539c27ad4d88d4d26fe7
Carlosayol <Carlosaolar@gmail.com>
2021-11-27 12:36:05 -0500

    add secondary generic type and deserialize

--- web/src/models/Collection.ts
+++ web/src/models/Collection.ts
@@ -3,11 +3,14 @@ import { User, UserProps } from "./User";
 import axios, { AxiosResponse } from 'axios'
 
 
-export class Collection {
-  models: User[] = []
+export class Collection<T, K> {
+  models: T[] = []
   events: Eventing = new Eventing()
 
-  constructor(public rootUrl: string) {}
+  constructor(
+    public rootUrl: string,
+    public deserialize: (json: K) => T
+  ) {}
 
   get on() {
     return this.events.on
@@ -20,10 +23,10 @@ export class Collection {
   fetch(): void {
     axios.get(this.rootUrl)
     .then((response: AxiosResponse) => {
-      response.data.forEach((value: UserProps) => {
-        const user = User.buildUser(value)
-        this.models.push(user)
+      response.data.forEach((value: K) => {
+        this.models.push(this.deserialize(value))
       })
+      this.trigger('change')
     })
   }
 }
