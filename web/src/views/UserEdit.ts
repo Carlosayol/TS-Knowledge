c6b02efb12c52725f093024dfb070fbc00801e9f
Carlosayol <Carlosaolar@gmail.com>
2021-12-05 11:25:15 -0500

    fix typeErrors

--- web/src/views/UserEdit.ts
+++ web/src/views/UserEdit.ts
@@ -1,4 +1,5 @@
 import { User, UserProps } from "../models/User"
+import { UserForm } from "./UserForm"
 import { UserShow } from "./UserShow"
 import { View } from "./View"
 
@@ -14,8 +15,8 @@ export class UserEdit extends View<User, UserProps> {
     const userShow = new UserShow(this.regions.userShow, this.model)
     userShow.render()
 
-    const UserForm = new UserForm(this.regions.userForm, this.model)
-    UserForm.render()
+    const userForm = new UserForm(this.regions.userForm, this.model)
+    userForm.render()
   }
 
   template(): string {
