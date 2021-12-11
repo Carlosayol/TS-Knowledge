d5bda0c63a1981550052f63a47ea11ceb2372edc
Carlosayol <Carlosaolar@gmail.com>
2021-12-11 11:36:44 -0500

    add login route

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -3,7 +3,28 @@ import { Request, Response, Router } from 'express'
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
-  res.send('Hola')
+  res.send('Home')
+})
+
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+  <form method="POST">
+    <div>
+      <label>Email</label>
+      <input name="email" />
+    </div>
+    <div>
+      <label>Password</label>
+      <input name="password" type="password" />
+    </div>
+    <button>Submit</button>
+  </form>
+  `)
+})
+
+router.post('/login', (req: Request, res: Response) => {
+  const { email, password } = req.body
+  res.send(email + password)
 })
 
 export { router }
