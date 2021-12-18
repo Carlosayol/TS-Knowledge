cb98cb0269a372698d05ae6340b2419561814b82
Carlosayol <Carlosaolar@gmail.com>
2021-12-18 10:01:33 -0500

    add boat class

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,3 +1,16 @@
+class Boat {
+  color: string = 'red'
+
+  get formattedColor(): string {
+    return `this boat color is ${this.color}`
+  }
+
+  @testDecorator
+  pilot(): void {
+    console.log('brumm')
+  }
+}
+
 function testDecorator( target: any, key: string ): void {
   console.log(target)
   console.log(key)
