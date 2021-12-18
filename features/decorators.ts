36f204fc8499a071dd2bed6b24282318095a38ed
Carlosayol <Carlosaolar@gmail.com>
2021-12-18 10:12:57 -0500

    create decorator factory to pass arguments to decorators

--- features/decorators.ts
+++ features/decorators.ts
@@ -5,14 +5,25 @@ class Boat {
     return `this boat color is ${this.color}`
   }
 
-  @logError
+  @logError('F')
   pilot(): void {
     throw new Error()
     console.log('brumm')
   }
 }
 
-function logError( target: any, key: string, desc: PropertyDescriptor ): void {
-  console.log(target)
-  console.log(key)
+function logError( errorMessage: string ) {
+  return  function( target: any, key: string, desc: PropertyDescriptor ): void {
+    const method = desc.value
+  
+    desc.value = function () {
+      try {
+        method()
+      } catch (e) {
+        console.log(errorMessage)
+      }
+    }
+  }
 }
+
+new Boat().pilot()
