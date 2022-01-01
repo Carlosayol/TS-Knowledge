336decdadb9d9f98bad7550a6ed5b214654a860b
Carlosayol <Carlosaolar@gmail.com>
2022-01-01 12:00:23 -0500

    add decorator and metadata use cases

--- features/metadata.ts
+++ features/metadata.ts
@@ -4,4 +4,29 @@ const plane = {
   color: "red"
 }
 
+@controller
+class Plane {
+  color: string = 'red'
+
+  @get('Volar')
+  fly(): void {
+    console.log('vrrrr')
+  }
+}
+
+function get(path: string) {
+  return function(target: Plane, key: string) {
+    Reflect.defineMetadata('path', path, target, key)
+  }
+}
+
+function controller(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const path = Reflect.getMetadata('path', target.prototype, key)
+  }
+}
+
+
+const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly')
+
 Reflect.defineMetadata('note', 'hello', plane)
