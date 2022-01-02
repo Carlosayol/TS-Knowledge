cc4656a8a7e50782cd8c5a2f683c754316341c23
Carlosayol <Carlosaolar@gmail.com>
2022-01-02 11:32:09 -0500

    create a approuter class for managing only one router

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,15 @@ import express from 'express'
 import { router } from './routes/loginRoutes'
 import bodyParser from 'body-parser'
 import cookieSession from 'cookie-session'
-import { router as controllerRouter } from './controllers/decorators/controller'
+import { AppRouter } from './AppRouter'
+
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieSession({ keys: ['laskdjf'] }))
-app.use(controllerRouter)
+app.use(router)
+app.use(AppRouter.getInstance())
 
 app.listen(3000, () => {
   console.log('Estoy atento pa')
