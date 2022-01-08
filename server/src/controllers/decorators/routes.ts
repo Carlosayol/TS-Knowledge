f5579c3b7e4703586dbe7ac37bf427a7ecee3cbd
Carlosayol <Carlosaolar@gmail.com>
2022-01-08 11:18:13 -0500

    create a routeBinder for multiple methods

--- server/src/controllers/decorators/routes.ts
+++ server/src/controllers/decorators/routes.ts
@@ -1,7 +1,15 @@
 import 'reflect-metadata'
 
-export function get(path: string) {
-  return function(target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata('path', path, target, key)
-  }
+function routBinder(method: string) {
+  return function(path: string) {
+    return function(target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata('path', path, target, key)
+      Reflect.defineMetadata('method', method, target, key)
+    }
+  }  
 }
+
+export const get = routBinder('get')
+export const post = routBinder('post')
+export const del = routBinder('del')
+export const put = routBinder('put')
