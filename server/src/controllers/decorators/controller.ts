437b017385b7d6bf38523dbb4822b50daaf75b2b
Carlosayol <Carlosaolar@gmail.com>
2022-01-10 10:05:32 -0500

    implement logic behind body validator

--- server/src/controllers/decorators/controller.ts
+++ server/src/controllers/decorators/controller.ts
@@ -6,9 +6,22 @@ import { Methods } from './Methods'
 
 function bodyValidator(keys: string): RequestHandler {
   return function(req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      res.status(422).send('Invalid Request')
+      return
+    }
 
+    for (let key of keys) {
+      if(!req.body[key]) {
+        res.status(422).send('Invalid request')
+        return
+      }
+    }
+
+    next()
   }
 }
+
 
 export function controller(routePrefix: string) {
   return function(target: Function) {
@@ -18,9 +31,12 @@ export function controller(routePrefix: string) {
       const path = Reflect.getMetadata(MetadataKeys.path, target.prototype, key)
       const method: Methods = Reflect.getMetadata(MetadataKeys.method, target.prototype, key)
       const middlewares = Reflect.getMetadata(MetadataKeys.middleware, target, key) || []
-      
+      const requiredBodyProps = Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) || []
+
+      const validator = bodyValidator(requiredBodyProps)
+
       if (path) {
-        router[method](`${routePrefix}${path}`, ...middlewares , routeHandler)
+        router[method](`${routePrefix}${path}`, ...middlewares , validator, routeHandler)
       }
     }
   }
